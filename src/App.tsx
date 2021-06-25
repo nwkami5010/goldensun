9eefe62f3fb4d25fddb2d36153f265a693bea0ec
nwkami <715190184@qq.com>
2021-06-25 16:56:38 +0800

    修复nav点击区域过小的问题

--- src/App.tsx
+++ src/App.tsx
@@ -3,65 +3,57 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import Money from './views/Money';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display:flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Statistics from './views/Statistics';
+
+import Tags from './views/Tags';
+
+import NoMatch from './views/NoMatch';
+
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+
+        <Route path="/tags">
+
+          <Tags/>
+
+        </Route>
+
+        <Route path="/money">
+
+          <Money/>
+
+        </Route>
+
+        <Route path="/statistics">
+
+          <Statistics/>
+
+        </Route>
+
+        <Redirect exact from="/" to="/money"/>
+
+        <Route path="*">
+
+          <NoMatch/>
+
+        </Route>
+
+      </Switch>
+
+
+
+
     </Router>
   );
 }
 
-function NoMatch() {
-  return (
-    <div>页面不存在，你丫输错地址了吧！</div>
-  );
-}
 
-function Statistics() {
-  return <h2>统计页面</h2>;
-}
-
-function Tags() {
-  return <h2>标签页面</h2>;
-}
-
-function Money() {
-  return <h2>记账页面</h2>;
-}
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,6 @@ import {Link} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
-require('icons/money.svg');
-require('icons/tag.svg');
-require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -13,17 +10,23 @@ const NavWrapper = styled.nav`
   > ul {
     display:flex;
     > li{
-      width: 33.3333%;
-      text-align:center;
+    width: 33.3333%;
+    text-align:center;
+      > a{
       display: flex;
       flex-direction: column;
       padding: 4px 0;
       justify-content: center;
       align-items: center;
+      
       .icon {
         width: 24px;      
         height: 24px;
       }
+      }
+      
+   
+
     }
   }
 `;
@@ -33,16 +36,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag"/>
-          <Link to="/tags">标签页</Link>
+          <Link to="/tags">
+            <Icon name="tag"/>
+            标签页
+          </Link>
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账页</Link>
+          <Link to="/money">
+            <Icon name="money"/>
+            记账页
+          </Link>
         </li>
         <li>
-          <Icon name="chart"/>
-          <Link to="/statistics">统计页</Link>
+          <Link to="/statistics">
+            <Icon name="chart"/>
+            统计页
+          </Link>
         </li>
       </ul>
     </NavWrapper>
