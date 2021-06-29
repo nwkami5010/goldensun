c559f4a310b872f8d0b7270fc24c26469d68d38c
nwkami <715190184@qq.com>
2021-06-29 17:13:47 +0800

    numberPad 功能 wip(work in process)

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 import React, {useState} from 'react';
 
 const CategorySection: React.FC = ()=>{
-  const categoryMap = {'-': '支出', '+': '收入'}
+  const categoryMap = {'-': '支出', '+':'收入'};
   const [category, setCategory] = useState('-')
-  const [categoryList] =useState<('+'|'-')[]>(['-','+'])
+  const [categoryList] = useState<('+' |'-')[]>(['-','+'])
   return (
     <Wrapper>
       <ul>
-        {categoryList.map(c =>
-          <li className={category === c ? 'selected':''}
-              onClick={()=> {setCategory(c);}}
+        { categoryList.map( c=>
+          <li key={c}
+              className={category=== c ? 'selected':''}
+              onClick={()=> {setCategory(c)}}
           >{categoryMap[c]}
           </li>
         )}
